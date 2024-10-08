bec4690f7fb2f4d72d3a85136820af8755bdd10a
michelle <michelle.graham@dkit.ie>
2024-10-08 10:26:46 +0100

    Added error handler

--- src/main/java/web_patterns/SampleSpringAppApplication.java
+++ src/main/java/web_patterns/SampleSpringAppApplication.java
@@ -1,4 +1,4 @@
-package web_patterns.samplespringapp;
+package web_patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/web_patterns/controllers/ErrorHandler.java
+++ src/main/java/web_patterns/controllers/ErrorHandler.java
@@ -1,4 +1,17 @@
 package web_patterns.controllers;
 
-public class ErrorController {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ErrorHandler implements ErrorController {
+    private final static String PATH = "/error";
+
+    @RequestMapping(PATH)
+    @ResponseBody
+    public String getErrorPath() {
+        return "No Mapping Found";
+    }
 }
