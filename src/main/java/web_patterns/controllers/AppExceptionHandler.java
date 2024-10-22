ab6f7420b1f7963fba88bbc4aa648b16213ed49d
michelle <michelle.graham@dkit.ie>
2024-10-22 01:03:00 +0100

    Feature: Added cross-cutting exception handling.

---
+++ src/main/java/web_patterns/controllers/AppExceptionHandler.java
@@ -0,0 +1,21 @@
+package web_patterns.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+    @ExceptionHandler(value = NullPointerException.class)
+    public String nullPointerHandler(Model model) {
+        model.addAttribute("err", "NullPointerException");
+        return "error";
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public String allOtherExceptionHandler(Model model, Exception ex) {
+        model.addAttribute("errType", ex.getClass());
+        model.addAttribute("errMsg", ex.getMessage());
+        return "error";
+    }
+}
