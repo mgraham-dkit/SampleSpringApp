fe1256f19806bf4f7df6f576d98a9af2cb2d42ba
michelle <michelle.graham@dkit.ie>
2024-12-03 02:17:30 +0000

    Feature: Internationalised order display  -- Order date now formatted according to user language

--- src/main/java/web_patterns/controllers/OrderController.java
+++ src/main/java/web_patterns/controllers/OrderController.java
@@ -1,7 +1,6 @@
 package web_patterns.controllers;
 
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
